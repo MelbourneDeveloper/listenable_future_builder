14b87f847714649626305dfdcab9a476b0dcb6dc
Christian Findlay <16697547+MelbourneDeveloper@users.noreply.github.com>
2023-04-11 10:34:15 +1000

    Add the code as is from the flutter PR

--- lib/listenable_future_builder.dart
+++ lib/listenable_future_builder.dart
@@ -1,7 +1,238 @@
 library listenable_future_builder;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+/// Builds a [Widget] when given a concrete value of a [AsyncSnapshot<T>] where
+/// `T` is a `Listenable`.
+///
+/// If the `child` parameter provided to the [AsyncListenableBuilder] is not
+/// null, the same `child` widget is passed back to this [ListenableFutureBuilder]
+/// and should typically be incorporated in the returned widget tree.
+///
+/// See also:
+///
+///  * [ListenableFutureBuilder], a widget which invokes this builder each time
+///  after the initial future completes and a value is emited on the [Listener].
+typedef AsyncListenableBuilder<T extends Listenable> = Widget Function(
+  BuildContext context,
+  Widget? child,
+  AsyncSnapshot<T> listenableSnapshot,
+);
+
+/// A widget that awaits initialization and then rebuilds when the it receives
+/// a notification from the `Listenable`. Use this widget with `ChangeNotifier`
+/// `ValueNotifier`, or other `Listenable` controllers to rebuild the widget
+/// when the value changes.
+///
+/// Given a [AsyncListenableBuilder<T>] and a [builder] which builds widgets from
+/// the state of the `Listenable`, this class will automatically register itself as a
+/// listener of the [Listenable] and call the [builder] with updated values
+/// when the value changes.
+///
+/// ## Performance optimizations
+///
+/// If your [builder] function contains a subtree that does not depend on the
+/// value of the [Listenable], it's more efficient to build that subtree
+/// once instead of rebuilding it on every animation tick.
+///
+/// If you pass the pre-built subtree as the [child] parameter, the
+/// [ListenableFutureBuilder] will pass it back to your [builder] function so
+/// that you can incorporate it into your build.
+///
+/// Using this pre-built child is entirely optional, but can improve
+/// performance significantly in some cases and is therefore a good practice.
+///
+/// {@tool snippet}
+///
+/// This sample demonstrates how you could use a [ListenableFutureBuilder] to wait
+/// for the initialization to complete, show a [CircularProgressIndicator] while
+/// waiting, and trigger rebuild on state changes without the need for setState
+///
+/// ```dart
+// import 'package:flutter/material.dart';
+// import 'package:notifier_builder/notifier_future_builder.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) => MaterialApp(
+//         theme: ThemeData(
+//           primarySwatch: Colors.blue,
+//         ),
+//         home: ListenableFutureBuilder(
+//           listenable: getValueNotifier,
+//           builder: (context, child, notifierSnapshot) => Scaffold(
+//             appBar: AppBar(
+//               title: const Text('ListenableFutureBuilder Example'),
+//             ),
+//             body: Center(
+//               child: notifierSnapshot.connectionState == ConnectionState.done
+//                   ? Column(
+//                       mainAxisAlignment: MainAxisAlignment.center,
+//                       children: <Widget>[
+//                         const Text(
+//                           'You have pushed the button this many times:',
+//                         ),
+//                         Text(
+//                           '${notifierSnapshot.data!.value}',
+//                           style: Theme.of(context).textTheme.headlineMedium,
+//                         ),
+//                       ],
+//                     )
+//                   : const CircularProgressIndicator.adaptive(),
+//             ),
+//             floatingActionButton: FloatingActionButton(
+//               onPressed: () => notifierSnapshot.data?.value++,
+//               tooltip: 'Increment',
+//               child: const Icon(Icons.add),
+//             ),
+//           ),
+//         ),
+//       );
+// }
+
+// ///This gets a [ValueNotifier<int>] after 3 seconds
+// Future<ValueNotifier<int>> getValueNotifier() =>
+//     Future<ValueNotifier<int>>.delayed(
+//       const Duration(seconds: 3),
+//       () => ValueNotifier<int>(0),
+//     );
+/// ```
+/// {@end-tool}
+///
+/// See also:
+///
+///  * [AnimatedBuilder], which also triggers rebuilds from a [Listenable]
+///    without passing back a specific value from a [ValueListenable].
+///  * [ValueListenableBuilder], which also triggers rebuilds from a [ValueListenable]
+///  * [NotificationListener], which lets you rebuild based on [Notification]
+///    coming from its descendant widgets rather than a [ValueListenable] that
+///    you have a direct reference to.
+class ListenableFutureBuilder<T extends Listenable> extends StatefulWidget {
+  /// Creates a [ListenableFutureBuilder].
+  ///
+  /// The [listenable] and [builder] arguments must not be null. [listenable]
+  /// should perform initialization logic for the [Listenable] and return the
+  /// instance.
+  ///
+  /// The [child] is optional but is good practice to use if part of the widget
+  /// subtree does not depend on the value of the [listenable].
+  const ListenableFutureBuilder({
+    required this.listenable,
+    required this.builder,
+    this.child,
+    this.disposeListenable,
+    super.key,
+  });
+
+  /// Instantiates the [Future] whose return value you depend on in order to build.
+  /// This should perform initialization logic for the [Listenable] and return it
+  final Future<T> Function() listenable;
+
+  /// Provides an opportunity to dispose of the [Listenable] when the
+  /// [_ListenableFutureBuilderState] is disposed.
+  final Future<T> Function(T listenable)? disposeListenable;
+
+  /// A [AsyncListenableBuilder] which builds a widget depending on the
+  /// [Listenable] such as a [ChangeNotifier]
+  ///
+  /// Exposes a [AsyncSnapshot] which contains the [Listenable] instance once the
+  /// initialization completes. It is important to check the `connectionState` of the
+  /// snapshot, and the `data` and `error` properties of the snapshot to determine
+  /// if the [Listenable] is ready to be used. The builder should usually return a
+  /// progress indicator until the connection state is [ConnectionState.done].
+  ///
+  /// Can incorporate the [child] parameter into the returned widget tree to improve
+  /// performance.
+  final AsyncListenableBuilder<T> builder;
+
+  /// A predefined widget which is passed back to the [builder].
+  ///
+  /// This argument is optional and can be null if the entire widget subtree the
+  /// [builder] builds depends on the value of the [Listenable]. For
+  /// example, in the case where the [Listenable] has a [String] and the
+  /// [builder] returns a [Text] widget with the current [String] value, there
+  /// would be no useful [child].
+  final Widget? child;
+
+  @override
+  State<ListenableFutureBuilder<T>> createState() =>
+      _ListenableFutureBuilderState<T>();
+}
+
+class _ListenableFutureBuilderState<T extends Listenable>
+    extends State<ListenableFutureBuilder<T>> {
+  Object? _activeCallbackIdentity;
+  late AsyncSnapshot<T> _snapshot;
+
+  ///Use this to access the last snapshot that was passed to the builder
+  AsyncSnapshot<T> get lastSnapshot => _snapshot;
+
+  @override
+  void initState() {
+    super.initState();
+    _snapshot = AsyncSnapshot<T>.nothing();
+    _subscribe();
+  }
+
+  @override
+  Widget build(BuildContext context) => Builder(
+        builder: (BuildContext context) =>
+            widget.builder(context, widget.child, _snapshot),
+      );
+
+  @override
+  void dispose() {
+    _unsubscribe();
+    if (_snapshot.data != null) {
+      widget.disposeListenable?.call(_snapshot.data!);
+    }
+    _snapshot = AsyncSnapshot<T>.nothing();
+    super.dispose();
+  }
+
+  void _handleChange() => setState(() {});
+
+  void _subscribe() {
+    final Object callbackIdentity = Object();
+    _activeCallbackIdentity = callbackIdentity;
+    widget.listenable().then<void>(
+      (T data) {
+        if (_activeCallbackIdentity == callbackIdentity) {
+          setState(() {
+            _snapshot = AsyncSnapshot<T>.withData(ConnectionState.done, data);
+            _snapshot.data!.addListener(_handleChange);
+          });
+        }
+      },
+      onError: (Object error, StackTrace stackTrace) {
+        if (_activeCallbackIdentity == callbackIdentity) {
+          setState(() {
+            _snapshot = AsyncSnapshot<T>.withError(
+              ConnectionState.done,
+              error,
+              stackTrace,
+            );
+          });
+        }
+      },
+    );
+
+    if (_snapshot.connectionState != ConnectionState.done) {
+      _snapshot = _snapshot.inState(ConnectionState.waiting);
+    }
+  }
+
+  void _unsubscribe() {
+    _activeCallbackIdentity = null;
+    if (_snapshot.connectionState == ConnectionState.done &&
+        _snapshot.data != null) {
+      _snapshot.data!.removeListener(_handleChange);
+    }
+  }
 }
--- test/listenable_future_builder_test.dart
+++ test/listenable_future_builder_test.dart
@@ -1,12 +1,245 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:listenable_future_builder/listenable_future_builder.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  testWidgets('ListenableFutureBuilder updates UI when future resolves',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: ListenableFutureBuilder<ValueNotifier<int>>(
+          listenable: () async => ValueNotifier<int>(42),
+          builder: (BuildContext context, Widget? child,
+                  AsyncSnapshot<ValueNotifier<int>> snapshot) =>
+              snapshot.connectionState == ConnectionState.done
+                  ? Text('${snapshot.data!.value}')
+                  : const CircularProgressIndicator(),
+        ),
+      ),
+    );
+
+    // Verify that CircularProgressIndicator is shown initially
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+    expect(find.text('42'), findsNothing);
+
+    // Wait for future to resolve
+    await tester.pumpAndSettle(const Duration(seconds: 1));
+
+    // Verify that Text widget with value 42 is shown after future resolves
+    expect(find.byType(CircularProgressIndicator), findsNothing);
+    expect(find.text('42'), findsOneWidget);
+  });
+
+  testWidgets('ListenableFutureBuilder updates UI when future errors',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: ListenableFutureBuilder<ValueNotifier<int>>(
+          listenable: () async => Future<ValueNotifier<int>>.delayed(
+            const Duration(milliseconds: 500),
+            () => throw Exception('Oops'),
+          ),
+          builder: (BuildContext context, Widget? child,
+              AsyncSnapshot<Listenable> snapshot) {
+            if (snapshot.connectionState == ConnectionState.done &&
+                snapshot.error != null) {
+              final dynamic error = snapshot.error as dynamic;
+              // ignore: avoid_dynamic_calls
+              final String errorMessage = 'Error: ${error.message}';
+              return Text(errorMessage);
+            } else {
+              return const CircularProgressIndicator();
+            }
+          },
+        ),
+      ),
+    );
+
+    // Verify that CircularProgressIndicator is shown initially
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+    expect(find.text('Error: Oops'), findsNothing);
+
+    // Wait for future to resolve
+    await tester.pumpAndSettle(const Duration(seconds: 1));
+
+    // Verify that Text widget with error message is shown after future errors
+    expect(find.byType(CircularProgressIndicator), findsNothing);
+    expect(find.text('Error: Oops'), findsOneWidget);
+  });
+
+  testWidgets('ListenableFutureBuilder updates UI when future returns null',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: ListenableFutureBuilder<ValueNotifier<String?>>(
+          listenable: () async => ValueNotifier<String?>(null),
+          builder: (BuildContext context, Widget? child,
+                  AsyncSnapshot<ValueNotifier<String?>> snapshot) =>
+              snapshot.connectionState == ConnectionState.done
+                  ? Text(
+                      snapshot.data?.value == null
+                          ? 'Nothing'
+                          : snapshot.data!.value!,
+                    )
+                  : const CircularProgressIndicator(),
+        ),
+      ),
+    );
+
+    // Verify that CircularProgressIndicator is shown initially
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+    expect(find.text('Nothing'), findsNothing);
+
+    // Wait for future to resolve
+    await tester.pumpAndSettle(const Duration(seconds: 1));
+
+    // Verify that Text widget with value null is shown after future resolves
+    expect(find.byType(CircularProgressIndicator), findsNothing);
+    expect(find.text('Nothing'), findsOneWidget);
+  });
+
+  testWidgets(
+      'ListenableFutureBuilder updates UI when future takes a while to resolve',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: ListenableFutureBuilder<ValueNotifier<int>>(
+          listenable: () async => Future<ValueNotifier<int>>.delayed(
+            const Duration(seconds: 2),
+            () => ValueNotifier<int>(42),
+          ),
+          builder: (BuildContext context, Widget? child,
+                  AsyncSnapshot<ValueNotifier<int>> snapshot) =>
+              snapshot.connectionState == ConnectionState.done
+                  ? Text('${snapshot.data!.value}')
+                  : const CircularProgressIndicator(),
+        ),
+      ),
+    );
+
+    // Verify that CircularProgressIndicator is shown initially
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+    expect(find.text('42'), findsNothing);
+
+    // Wait for future to resolve
+    await tester.pumpAndSettle(const Duration(seconds: 3));
+
+    // Verify that Text widget with value 42 is shown after future resolves
+    expect(find.byType(CircularProgressIndicator), findsNothing);
+    expect(find.text('42'), findsOneWidget);
+  });
+
+  testWidgets('ListenableFutureBuilder updates UI when the notifier is changed',
+      (WidgetTester tester) async {
+    final ValueNotifier<int> notifier = ValueNotifier<int>(0);
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: ListenableFutureBuilder<ValueNotifier<int>>(
+          listenable: () async => notifier,
+          builder: (BuildContext context, Widget? child,
+                  AsyncSnapshot<ValueNotifier<int>> snapshot) =>
+              snapshot.connectionState == ConnectionState.done
+                  ? Text('${snapshot.data!.value}')
+                  : const CircularProgressIndicator(),
+        ),
+      ),
+    );
+
+    // Verify that CircularProgressIndicator is shown initially
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+    expect(find.text('0'), findsNothing);
+
+    // Wait for future to resolve
+    await tester.pumpAndSettle(const Duration(seconds: 1));
+
+    // Verify that Text widget with value 0 is shown after future resolves
+    expect(find.byType(CircularProgressIndicator), findsNothing);
+    expect(find.text('0'), findsOneWidget);
+
+    // Increment the value of the notifier
+    notifier.value = 1;
+
+    // Verify that Text widget with value 1 is shown after notifier is changed
+    await tester.pumpAndSettle();
+    expect(find.text('1'), findsOneWidget);
+  });
+
+  ///Notes: we need to confirm that the builder drops its reference to the
+  ///Listenable when the widget is disposed. If the state holds onto a
+  ///previous AsyncSnapshot, it will hold onto the Listenable as well. This
+  ///test ensures that the State doesn't hold onto the Listenable after
+  ///disposal.
+  testWidgets('ListenableFutureBuilder disposes correctly',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: ListenableFutureBuilder<ValueNotifier<int?>>(
+          listenable: () async => ValueNotifier<int?>(3),
+          builder: (BuildContext context, Widget? child,
+                  AsyncSnapshot<ValueNotifier<int?>> snapshot) =>
+              snapshot.connectionState == ConnectionState.done
+                  ? Text('${snapshot.data!.value}')
+                  : const CircularProgressIndicator(),
+        ),
+      ),
+    );
+
+    //_ListenableFutureBuilderState is private so we cannot access the state
+    //without dynamic
+    final dynamic state =
+        tester.state(find.byType(ListenableFutureBuilder<ValueNotifier<int?>>));
+
+    //Triggers disposal
+    await tester.pumpWidget(const SizedBox());
+
+    final AsyncSnapshot<ValueNotifier<int?>> snapshot =
+        //We have lastSnapshot so we can test this. Alternative approaches to
+        //testing for this are absolutely welcome, and we can remove this
+        //getter if it is too problematic.
+        // ignore: avoid_dynamic_calls
+        state.lastSnapshot as AsyncSnapshot<ValueNotifier<int?>>;
+
+    //Verify the state does not hold onto the Listenable after disposal
+    expect(snapshot.data, isNull);
+    expect(snapshot.connectionState, ConnectionState.none);
+  });
+
+  testWidgets('Dispose calls disposeListenable when the widget is disposed',
+      (WidgetTester tester) async {
+    // Create a ValueNotifier to be used in the test
+    final ValueNotifier<int> valueNotifier = ValueNotifier<int>(0);
+    bool disposeListenableCalled = false;
+
+    // Define the disposeListenable function
+    Future<ValueNotifier<int>> disposeListenable(
+        ValueNotifier<int> listenable) {
+      disposeListenableCalled = true;
+      return Future<ValueNotifier<int>>.value(listenable);
+    }
+
+    // Define a simple widget tree with the ListenableFutureBuilder
+    final MaterialApp app = MaterialApp(
+      home: ListenableFutureBuilder<ValueNotifier<int>>(
+        listenable: () async => valueNotifier,
+        builder: (BuildContext context, Widget? child,
+            AsyncSnapshot<ValueNotifier<int>> listenableSnapshot) {
+          return const Text('Test');
+        },
+        disposeListenable: disposeListenable,
+      ),
+    );
+
+    // Pump the widget
+    await tester.pumpWidget(app);
+
+    // Wait for the async operation to complete
+    await tester.pumpAndSettle();
+
+    // Trigger a rebuild and dispose of the widget
+    await tester.pumpWidget(const SizedBox());
+
+    // Check if disposeListenable was called
+    expect(disposeListenableCalled, true);
   });
 }
